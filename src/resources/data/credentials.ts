47d73e12a6a3e4c4238e98cf9e7b022b4abd2684
longle <longle159268@gmail.com>
2026-05-22 15:45:10 +0700

    fix: update credential

--- src/resources/data/credentials.ts
+++ src/resources/data/credentials.ts
@@ -1,15 +1,7 @@
 export const credentials = {
   admin: {
-    username: "superadmin",
-    password: "1234567@Abc",
-  },
-  BGĐ: {
-    username: "giamdoc",
-    password: "12345678@Abc",
-  },
-  P1: {
-    username: "p1hvktqs",
-    password: "12345678@Abc",
+    username: "hvktqs",
+    password: "123456@Abc",
   },
   P2qldt: {
     username: "p2qldt",
@@ -18,14 +10,6 @@ export const credentials = {
   P2qlct: {
     username: "p2qlct",
     password: "1234567@Abc",
-  },
-  P4: {
-    username: "p4hvktqs",
-    password: "12345678@Abc",
-  },
-  P6: {
-    username: "p6",
-    password: "12345678@Abc",
   },
   P7: {
     username: "p7hvktqs",
@@ -38,26 +22,6 @@ export const credentials = {
   P13nd: {
     username: "p13nd",
     password: "1234567@Abc",
-  },
-  KV: {
-    username: "khukv2025",
-    password: "12345678@Abc",
-  },
-  V4: {
-    username: "bomon2025",
-    password: "12345678@Abc",
-  },
-  GV: {
-    username: "gv2025",
-    password: "12345678@Abc",
-  },
-  HV: {
-    username: "hv2025",
-    password: "12345678@Abc",
-  },
-  SV: {
-    username: "sv2025",
-    password: "12345678@Abc",
   },
   Swantech: {
     username: "swantech",
